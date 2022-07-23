3588164320bff58d222c8039a76972f75d23a13c
thaogiang.90@gmail.com <thaogiang.90@gmail.com>
2022-07-23 21:38:32 +0700

    143 Implementing custom ui elements for a progress bar to click counterlock wise

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -3,18 +3,19 @@ package com.example.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
-import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
 class ExerciseActivity : AppCompatActivity() {
-
-    private var  binding : ActivityExerciseBinding?= null
+    //Todo 1 Create 1 binding variable
+    private var binding: ActivityExerciseBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //Todo 2 inflate the layout
         binding = ActivityExerciseBinding.inflate(layoutInflater)
+        //Todo 3 pass in binding?.root in the content view
         setContentView(binding?.root)
+        //Todo 5 then set support action bả and get tooBarExerciser using the binding variable
         setSupportActionBar(binding?.toolbarExcise)
-        if(supportActionBar!=null){
+        if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
         binding?.toolbarExcise?.setNavigationOnClickListener {
